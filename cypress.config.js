14e98b8c918a0fea34cd76d9482b7c03e3bc6498
khamis1992 <khamis-1992@hotmail.com>
2025-04-30 17:04:31 +0300

    up

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: "67ij87",
   e2e: {
     baseUrl: 'http://localhost:5173',
     supportFile: 'e2e/support/e2e.js',
