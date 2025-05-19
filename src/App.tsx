330d91b7466d0fcc0ee4f7406da76a7aaebeceee
khamis1992 <khamis-1992@hotmail.com>
2025-05-19 11:25:08 +0300

    Add customer mobile portal

--- src/App.tsx
+++ src/App.tsx
@@ -72,6 +72,7 @@ import ScheduledReports from "./pages/ScheduledReports";
 
 // System Settings pages
 import SystemSettings from "./pages/SystemSettings";
+import CustomerPortal from "./pages/CustomerPortal";
 
 import initializeApp from "./utils/app-initializer";
 
@@ -113,7 +114,16 @@ function App() {
                       <Route path="forgot-password" element={<ForgotPassword />} />
                       <Route path="reset-password" element={<ResetPassword />} />
                     </Route>
-                    
+
+                    <Route
+                      path="/portal"
+                      element={
+                        <ProtectedRoute>
+                          <CustomerPortal />
+                        </ProtectedRoute>
+                      }
+                    />
+
                     {/* Protected Routes */}
                     <Route
                       path="/*"
