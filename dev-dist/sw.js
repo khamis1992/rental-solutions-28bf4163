a48a3d9211f89726bd80c26069d96426b466ea7e
khamis1992 <khamis-1992@hotmail.com>
2025-06-29 01:23:02 +0300

    10

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-4e60344b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qgvrm98n1q8"
+    "revision": "0.vu41s2hmbf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
